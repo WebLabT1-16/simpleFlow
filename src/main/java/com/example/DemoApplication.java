d34da56166b2582a574bc58e882aa1fb1136fe37
Josep Roure <roure@tecnocampus.cat>
2016-10-21 07:55:16 +0200

    first commit

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import org.apache.catalina.Context;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+		factory.setTomcatContextCustomizers(Arrays.asList(new CustomCustomizer()));
+		return factory;
+	}
+
+	static class CustomCustomizer implements TomcatContextCustomizer {
+		@Override
+		public void customize(Context context) {
+			context.setUseHttpOnly(false);
+		}
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
